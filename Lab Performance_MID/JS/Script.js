e0e4bd9a0ab43a5c746cbf01c6a80725c7babfaf
rifatroni71200-blip <rifatroni71200@gmail.com>
2026-03-16 15:52:08 +0600

    validation to prevent negative quantity input

--- Lab Performance_MID/JS/Script.js
+++ Lab Performance_MID/JS/Script.js
@@ -2,3 +2,8 @@ function calculate_total() {
     var price = document.getElementById("UnitPrice").value;
     var quantity = document.getElementById("Quantity").value;
 }
+if(quantity < 0) {
+    alert("Quantity cannot be negative");
+    document.getElementById("Quantity").value = 0;
+    quantity = 0;
+}
