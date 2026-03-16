d7a813371aea3d3c759e9607a08f4be1d8d69a75
rifatroni71200-blip <rifatroni71200@gmail.com>
2026-03-16 15:57:15 +0600

    final

--- Lab Performance_MID/JS/Script.js
+++ Lab Performance_MID/JS/Script.js
@@ -1,14 +1,21 @@
-function calculate_total() {
+function calculate_total()
+{
     var price = document.getElementById("UnitPrice").value;
     var quantity = document.getElementById("Quantity").value;
-}
-if(quantity < 0) {
-    alert("Quantity cannot be negative");
-    document.getElementById("Quantity").value = 0;
-    quantity = 0;
-}
-var total = price * quantity * 30;
-document.getElementById("TotalPrice").value = total;
-if(total > 1000) {
-    alert("You are eligible for a gift coupon!");
+
+    if(quantity < 0)
+    {
+        alert("Quantity cannot be negative");
+        document.getElementById("Quantity").value = 0;
+        quantity = 0;
+    }
+
+    var total = price * quantity * 30;
+
+    document.getElementById("TotalPrice").value = total;
+
+    if(total > 1000)
+    {
+        alert("You are eligible for a gift coupon!");
+    }
 }
